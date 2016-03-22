affce9c1ce5bc8bdca4c59627d6e902e9cb4a9ae
Robert Starrett <Robert Starrett>
2016-03-22 10:15:03 -0500

    added models

---
+++ ReportingCapstone/ReportingCapstone/Models/DownTimeEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManufacturingReporting.Models
+{
+    public class DownTimeEvent
+    {
+        public int Id { get; set; }
+        public int ProblemCode { get; set; }
+        public int DepartmentCode { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
---
+++ ReportingCapstone/ReportingCapstone/Models/ManufacturingDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ManufacturingReporting.Models
+{
+    public class ManufacturingDBContext : DbContext
+    {
+        public ManufacturingDBContext() : base("defaultConnection")
+        {
+
+        }
+
+        public DbSet<DownTimeEvent> DowntimeLog { get; set; }
+        public DbSet<Departments> Departments { get; set; }
+        public DbSet<DownTimeTypes> ErrorCodes { get; set; }
+
+    }
+}
