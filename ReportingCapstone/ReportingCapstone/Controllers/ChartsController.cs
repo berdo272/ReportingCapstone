b7a0d53d77a9ec59b97d315acb981134b6fc67ad
Robert Starrett <Robert Starrett>
2016-03-23 14:17:45 -0500

    Added Charts Controller and related views, Dropped stock google charts implementation into chart views

---
+++ ReportingCapstone/ReportingCapstone/Controllers/ChartsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportingCapstone.Controllers
+{
+    public class ChartsController : Controller
+    {
+        // GET: Charts
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Perato()
+        {
+            return View();
+        }
+        public ActionResult Trend()
+        {
+            return View();
+        }
+    }
+}
