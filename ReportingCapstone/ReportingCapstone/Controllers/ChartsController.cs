0132b29df0c7bde0140c26ae0c552b5c95bb24d6
Robert Starrett <Robert Starrett>
2016-03-25 16:37:27 -0500

    Bug fixes and algorithm tweaks

--- ReportingCapstone/ReportingCapstone/Controllers/ChartsController.cs
+++ ReportingCapstone/ReportingCapstone/Controllers/ChartsController.cs
@@ -13,33 +13,33 @@ namespace ReportingCapstone.Controllers
         // GET: Charts
         public ActionResult Index()
         {
-            
+
             return View();
         }
         public ActionResult Perato()
         {
-            List<KeyValuePair<int,string>> sortKV = new List<KeyValuePair<int, string>>();
+            List<KeyValuePair<int, string>> sortKV = new List<KeyValuePair<int, string>>();
 
             List<string> Keys = new List<string>();
             List<int> Values = new List<int>();
 
-            foreach(DownTimeType DTT in db.DownTimeTypes)
+            foreach (DownTimeType DTT in db.DownTimeTypes)
             {
                 int durationTotal = 0;
                 List<DownTimeEvent> Events = db.DownTimeEvents.Where(O => O.DownTimeTypeId == DTT.Id & O.Date.Month == DateTime.Now.Month).ToList();
 
-                foreach(DownTimeEvent x in Events)
+                foreach (DownTimeEvent x in Events)
                 {
                     durationTotal += x.Duration;
                 }
 
-                sortKV.Add(new KeyValuePair<int, string>( durationTotal, DTT.Description));            
+                sortKV.Add(new KeyValuePair<int, string>(durationTotal, DTT.Description));
 
             }
 
             sortKV = sortKV.OrderByDescending(o => o.Key).ToList();
 
-            foreach(KeyValuePair<int,string> x in sortKV)
+            foreach (KeyValuePair<int, string> x in sortKV)
             {
                 Values.Add(x.Key);
                 Keys.Add(x.Value);
@@ -53,22 +53,78 @@ namespace ReportingCapstone.Controllers
         }
         public ActionResult Trend()
         {
+            int daysInMonth = 0;
 
-            List<DownTimeEvent> sortedEvents = db.DownTimeEvents.ToList();
-            sortedEvents = sortedEvents.OrderBy(o => o.Date).ToList();
+            switch (DateTime.Now.Month)
+            {
+                case 1:
+                    daysInMonth = 31;
+                    break;
+                case 2:
+                    if (DateTime.Now.Year % 4 == 0)
+                    {
+                        daysInMonth = 29;
+                        break;
+                    }
+                    else
+                    {
+                        daysInMonth = 28;
+                        break;
+                    }
+                case 3:
+                    daysInMonth = 31;
+                    break;
+                case 4:
+                    daysInMonth = 30;
+                    break;
+                case 5:
+                    daysInMonth = 31;
+                    break;
+                case 6:
+                    daysInMonth = 30;
+                    break;
+                case 7:
+                    daysInMonth = 31;
+                    break;
+                case 8:
+                    daysInMonth = 31;
+                    break;
+                case 9:
+                    daysInMonth = 30;
+                    break;
+                case 10:
+                    daysInMonth = 31;
+                    break;
+                case 11:
+                    daysInMonth = 30;
+                    break;
+                case 12:
+                    daysInMonth = 31;
+                    break;
 
+            }
 
             List<decimal> percentages = new List<decimal>();
             List<int> dates = new List<int>();
+            List<DownTimeEvent> Events = db.DownTimeEvents.Where(o => o.Date.Month == DateTime.Now.Month).ToList();
 
-            foreach (DownTimeEvent DTE in sortedEvents)
+            for(int i = 1; i <= daysInMonth; i++)
             {
-                decimal dec = (DTE.Duration / 435m);
+                decimal dailyDowntimePercent = 0;
 
-                percentages.Add((DTE.Duration / 435m));
-                dates.Add(DTE.Date.Year);
-                dates.Add((DTE.Date.Month) - 1);
-                dates.Add(DTE.Date.Day);
+                foreach(DownTimeEvent dte in Events)
+                {
+                    if(dte.Date.Day == i)
+                    {
+                        dailyDowntimePercent += (dte.Duration / 435m);
+                    }
+                }
+
+                dates.Add(DateTime.Now.Year);
+                dates.Add((DateTime.Now.Month) - 1);
+                dates.Add(i);
+
+                percentages.Add(dailyDowntimePercent);
             }
 
             ViewBag.percentages = percentages;
--- ReportingCapstone/ReportingCapstone/Migrations/ConfigurationReportingCapstoneDB.cs
+++ ReportingCapstone/ReportingCapstone/Migrations/ConfigurationReportingCapstoneDB.cs
@@ -6,6 +6,7 @@ namespace ReportingCapstone.Migrations
     using Microsoft.AspNet.Identity.EntityFramework;
     using ReportingCapstone.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -19,7 +20,92 @@ namespace ReportingCapstone.Migrations
 
         protected override void Seed(ReportingCapstone.Models.ReportingCapstoneDBContext context)
         {
+            List<DownTimeType> downTimeTypes = new List<DownTimeType>
+            {
+                new DownTimeType
+                {
+                    Description = "Component Shortage"
+                },
+                new DownTimeType
+                {
+                    Description = "Paint Popper Down"
+                },
+                new DownTimeType
+                {
+                    Description = "Printer Ribbon Replacement"
+                },
+                new DownTimeType
+                {
+                    Description = "Waiting For Material Handler"
+                },
+                new DownTimeType
+                {
+                    Description = "Change Over"
+                },
+                new DownTimeType
+                {
+                    Description = "Copper Crush Washer Replacement"
+                },
+                new DownTimeType
+                {
+                    Description = "Evac and Fill Failure"
+                },
+                new DownTimeType
+                {
+                    Description = "Printer Label Change"
+                },
+                new DownTimeType
+                {
+                    Description = "Waiting for Oil Drum Replacement"
+                },
+                new DownTimeType
+                {
+                    Description = "Problem Solving With Engineer"
+                },
+                new DownTimeType
+                {
+                    Description = "Piston Pusher Inop"
+                },
+                new DownTimeType
+                {
+                    Description = "6 Piston tester Down"
+                },
+            };
+            downTimeTypes.ForEach(b => context.DownTimeTypes.Add(b));
 
+            context.SaveChanges();
+
+            List<Department> departments = new List<Department>
+            {
+                new Department
+                {
+                    DepartmentName = "Tricept"
+                },
+                new Department
+                {
+                    DepartmentName = "A8_0108"
+                },
+                new Department
+                {
+                    DepartmentName = "A8_0206"
+                },
+                new Department
+                {
+                    DepartmentName = "A8_0212"
+                },
+                new Department
+                {
+                    DepartmentName = "B5_0106"
+                },
+                new Department
+                {
+                    DepartmentName = "H6_0219"
+                },
+            };
+            departments.ForEach(b => context.Departments.Add(b));
+
+            context.SaveChanges();
+                     
         }
     }
 }
